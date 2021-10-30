395073fafed8b7d12bc2bd88dfe279b6c52267f9
Kripmag <73032620+Kripmag@users.noreply.github.com>
2021-10-30 10:22:50 +0300

    Add IsLeapYear in main

--- ConsoleApplication2/ConsoleApplication2/main.cpp
+++ ConsoleApplication2/ConsoleApplication2/main.cpp
@@ -1,7 +1,17 @@
 ﻿#include <iostream>
+using namespace std;
+
+bool IsLeapYear(int year)
+{
+	return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+}
 
 
 int main()
 {
-    std::cout << "HI GIT!\n";
+	cout << IsLeapYear(2000) << " 2000" << '\n';
+	cout << IsLeapYear(2004) << " 2004" << '\n';
+	cout << IsLeapYear(100) << " 100" << '\n';
+	cout << IsLeapYear(200) << " 200" << '\n';
+	cout << IsLeapYear(104) << " 104" << '\n';
 }
