03280bdd54c89b762a3898ca7496f84ea640c426
Kripmag <73032620+Kripmag@users.noreply.github.com>
2021-10-30 10:53:08 +0300

    Revert "Add SecondsInMinute"
    This reverts commit 18bdcf183115c83362fe90144a0799e3a46016c3.

--- ConsoleApplication2/ConsoleApplication2/main.cpp
+++ ConsoleApplication2/ConsoleApplication2/main.cpp
@@ -1,22 +1,12 @@
 ﻿#include <iostream>
 using namespace std;
 
-/// <summary>
-/// Возвращает високостность года
-/// </summary>
-/// <returns> количество секунд в минуте</returns>
 bool IsLeapYear(int year)
 {
 	return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
 }
-/// <summary>
-/// Возвращает количество секунд в минуте
-/// </summary>
-/// <returns> количество секунд в минуте</returns>
-int SecondsInMinute()
-{
-	return  60;
-}
+
+
 int main()
 {
 	cout << IsLeapYear(2000) << " 2000" << '\n';
